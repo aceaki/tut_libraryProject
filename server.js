e6cf272c9eda13388ac85a6d76e2e66687feccf9
aceaki <alexceaki.v@gmail.com>
2020-04-22 14:54:06 +0100

    autors route

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express()
 const expressLayouts =  require('express-ejs-layouts')
 
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 
 mongoose.connect(process.env.DATABASE_URL, { 
@@ -25,14 +26,21 @@ db.once('open', () => console.log('connected to database'))
 
 
 
-const indexRouter = require('./routes/index')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname +'/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit : '10mb', extended: false}))
 
+
+const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
 app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
+
+
 
 app.listen(process.env.PORT || 3000)
